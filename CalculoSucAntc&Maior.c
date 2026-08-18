818469bdaee8b8b450396d871e86e3555eaac931
Davi Emmanuel <daviemmanuel37@gmail.com>
2026-08-18 09:12:46 -0300

    Add program to calculate predecessor, successor, and max
    o programa calcula o antecessor e sucessor do numero e o maior entre três números

---
+++ CalculoSucAntc&Maior.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(int argc, char *argv[]) {
+   
+    int n, antecessor, sucessor;
+    printf("entre com o valor de N");
+    scanf("%d", &n);
+    sucessor = n+1;
+    antecessor = n-1;
+    printf("o numero &d, seu antecessor %d e seu sucessor %d ", sucessor, antecessor);
+return 0;
+  
+
+ int a,b,c, maiorTemp, maior;
+   printf("insira tres valores para identificar o maior: ");
+   scanf("%d %d %d",&a,&b,&c);
+   
+   maiorTemp = ((a+b+abs(a-b))/2);
+ 
+   maior = ((maiorTemp+c+abs(maiorTemp-c))/2);
+ 
+   printf("o maior entre |%d|%d|%d| = %d ", a,b,c, maior);
+
+  return 0;
+
+}
